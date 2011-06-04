0c4d62d84d01241a6e13247b9f8445eb4b554fd1
Marty Schoch <marty.schoch@gmail.com>
2011-06-04 18:53:40 -0400

    modified to implement dependencies

---
+++ MobileCouchbase/Scripts/createUserPackage.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#calculate SDK version
+SDK_VERSION=$(echo ${SDK_NAME} | grep -o '.\{3\}$')
+
+#build iphone native first
+SDK_TO_BUILD=iphoneos${SDK_VERSION}
+ARCH="armv6 armv7"
+
+xcodebuild -workspace "${SRCROOT}/../Couchbase.xcworkspace/" -scheme iErl14-iphoneos -configuration "${CONFIGURATION}" -sdk "${SDK_TO_BUILD}" -arch "${ARCH}" ${ACTION} RUN_CLANG_STATIC_ANALYZER=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+
+xcodebuild -workspace "${SRCROOT}/../Couchbase.xcworkspace/" -scheme iMonkey-iphoneos -configuration "${CONFIGURATION}" -sdk "${SDK_TO_BUILD}" -arch "${ARCH}" ${ACTION} RUN_CLANG_STATIC_ANALYZER=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+
+xcodebuild -workspace "${SRCROOT}/../Couchbase.xcworkspace/" -scheme Couchbase-iphoneos -configuration "${CONFIGURATION}" -sdk "${SDK_TO_BUILD}" -arch "${ARCH}" ${ACTION} RUN_CLANG_STATIC_ANALYZER=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+
+
+#now build iphone simulator
+SDK_TO_BUILD=iphonesimulator${SDK_VERSION}
+ARCH="i386"
+
+xcodebuild -workspace "${SRCROOT}/../Couchbase.xcworkspace/" -scheme iErl14-iphonesimulator -configuration "${CONFIGURATION}" -sdk "${SDK_TO_BUILD}" -arch "${ARCH}" ${ACTION} RUN_CLANG_STATIC_ANALYZER=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+
+xcodebuild -workspace "${SRCROOT}/../Couchbase.xcworkspace/" -scheme iMonkey-iphonesimulator -configuration "${CONFIGURATION}" -sdk "${SDK_TO_BUILD}" -arch "${ARCH}" ${ACTION} RUN_CLANG_STATIC_ANALYZER=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+
+xcodebuild -workspace "${SRCROOT}/../Couchbase.xcworkspace/" -scheme Couchbase-iphonesimulator -configuration "${CONFIGURATION}" -sdk "${SDK_TO_BUILD}" -arch "${ARCH}" ${ACTION} RUN_CLANG_STATIC_ANALYZER=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+
+#finally build the bundle
+
+xcodebuild -workspace "${SRCROOT}/../Couchbase.xcworkspace/" -scheme Couchbase.bundle -configuration "${CONFIGURATION}" -sdk "${SDK_TO_BUILD}" -arch "${ARCH}" ${ACTION} RUN_CLANG_STATIC_ANALYZER=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+
+
+if [ $ACTION = "build" ]
+then
+#now build fat binary
+lipo -create -output ${BUILT_PRODUCTS_DIR}/libCouchbase.a ${BUILT_PRODUCTS_DIR}/libCouchbase-iphoneos.a ${BUILT_PRODUCTS_DIR}/libCouchbase-iphonesimulator.a
+
+#now build zip file
+cd ${BUILT_PRODUCTS_DIR}/;
+zip -r iOSCouchbase.zip include/Couchbase.h Couchbase.bundle libCouchbase.a
+
+elif [ $ACTION = "clean" ]
+then
+
+# -f is required because it seems to call clean for all configurations (even ones you haven't built)
+rm -f ${BUILT_PRODUCTS_DIR}/libCouchbase.a
+rm -f ${BUILT_PRODUCTS_DIR}/iOSCouchbase.zip
+
+fi
